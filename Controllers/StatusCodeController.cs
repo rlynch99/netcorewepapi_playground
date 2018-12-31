b973374654f1c355b2b60484b160c6bdbd01a85a
Sara Lynch <rlynch99@hotmail.com>
2018-12-31 06:44:56 -0600

    Initial Checkin

---
+++ Controllers/StatusCodeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WebApiWithSecurity.Controllers
+{
+    [Route("[controller]")]
+    public class StatusCodeController : Controller
+    {
+        private ILogger<StatusCodeController> _iLogger;
+
+        public StatusCodeController(ILogger<StatusCodeController> ilogger)
+        {
+            _iLogger = ilogger;
+        }
+
+        [HttpGet("/StatusCode/{StatusCode}")]
+        public IActionResult Index(int statusCode)
+        {
+            var reExecuteFeature = HttpContext.Features.Get<IStatusCodeReExecuteFeature>();
+            _iLogger.LogInformation("Unexpected StatusCode:{StatusCode}, page: {OriginalPath})", statusCode, reExecuteFeature.OriginalPath);
+            return View(statusCode);
+        }
+    }
+}
---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiWithSecurity.Controllers
+{
+    [Route("api/[controller]")]
+    //[Authorize]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<Value> Get()
+        {
+            return new Value[] { new Value { Id = 1, Title = "Toast is great" }, new Value { Id = 2, Title = "Use Vegemite" } };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<Value> Get(int id)
+        {
+            return new Value { Id = id, Title = $"value {id}" };
+        }
+
+        // POST api/values
+        [HttpPost]
+        [Produces(typeof(Value))]
+        public ActionResult Post([FromBody] Value value)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            return CreatedAtAction("Get", new { id = value.Id }, value);
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+    public class Value
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+    }
+}
